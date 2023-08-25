535c10198b8adc10151cfd500028945ef25d4810
Nafets94 <effsty@gmail.com>
2023-08-26 01:16:24 +0300

    added pagination to our list

--- frontend/web-app/app/nav/auctions/Listings.tsx
+++ frontend/web-app/app/nav/auctions/Listings.tsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import AuctionCard from './AuctionCard';
 import { Auction, PagedResult } from '@/types';
+import AppPagination from '@/app/components/AppPagination';
 
 async function getData(): Promise<PagedResult<Auction>> {
-    const res = await fetch('http://localhost:6001/search?pageSize=10');
-    if (!res.ok) throw new Error('Failed to fetched data');
-    return res.json();
+  const res = await fetch('http://localhost:6001/search?pageSize=3');
+  if (!res.ok) throw new Error('Failed to fetched data');
+  return res.json();
 }
 
 export default async function Listings() {
   const data = await getData();
   return (
-    <div className='grid grid-cols-4 gap-6'>
-      {data && data.result.map(auction => (
-        <AuctionCard key={auction.id} auction={auction}/>
-      ))}
-    </div>
+    <>
+      <div className='grid grid-cols-4 gap-6'>
+        {data && data.result.map(auction => (
+          <AuctionCard key={auction.id} auction={auction} />
+        ))}
+      </div>
+      <div className='flex justify-center mt-4'>
+        <AppPagination currentPage={1} pageCount={data.pageCount} />
+      </div>
+    </>
   )
 }

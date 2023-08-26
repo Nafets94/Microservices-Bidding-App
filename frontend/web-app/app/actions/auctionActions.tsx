795a724db69b5681452616e697d8b5737a6cd5f8
Nafets94 <effsty@gmail.com>
2023-08-26 23:23:49 +0300

    added server function in client component

---
+++ frontend/web-app/app/actions/auctionActions.tsx
@@ -0,0 +1,9 @@
+'use server'
+
+import { Auction, PagedResult } from "@/types";
+
+export async function getData(pageNumber: number = 1): Promise<PagedResult<Auction>> {
+    const res = await fetch(`http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`);
+    if (!res.ok) throw new Error('Failed to fetched data');
+    return res.json();
+}
